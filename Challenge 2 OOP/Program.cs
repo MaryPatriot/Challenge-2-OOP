8acfae77d751490346d355d04390bf597b7aea55
Mary <mary.vatanparast@gmail.com>
2021-02-03 16:35:03 +0100

    Add project files.

---
+++ Challenge 2 OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rec1 = new Rectangle(4, 5 );
+
+            Console.WriteLine(Rec1.length);
+            Console.WriteLine(Rec1.width);
+            Console.WriteLine(Rec1.Area());
+
+
+
+
+        }
+    }
+}
---
+++ Challenge 2 OOP/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2_OOP
+{
+    class Rectangle
+    {
+        public double length;
+        public double width;
+       
+
+       
+        public Rectangle(double Plength, double Pwidth)
+        {
+            length = Plength;
+             width = Pwidth;
+            
+        }
+
+        public double Area()
+        {
+            double area = length * width;
+            return area;
+        }
+
+        
+    }
+}
